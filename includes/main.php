21da4cc204d1522a026d658758bf6bd9b694f0ff
Breyson Olivera <breyolic@gmail.com>
2018-04-19 00:19:38 -0500

    Main parte II solo html, css modificado por simple tema de comodidad del icono chef

--- includes/main.php
+++ includes/main.php
@@ -1,7 +1,7 @@
     <section class=ed-container>
         <div class="ed-container ed-item m-40">
             <div class="ed-item">
-                <img src="assets/chef.svg" alt="">
+                <img class=icono src="assets/chef.svg" alt="">
             </div>
             <div class="ed-item">
                 <p>Madison Restaurant</p>
@@ -43,7 +43,7 @@
         </div>
         <div class="ed-container ed-item m-40">
             <div class="ed-item">
-                <img src="assets/chef.svg" alt="">
+                <img class=icono src="assets/chef.svg" alt="">
             </div>
             <div class="ed-item">
                 <p>Madison Restaurant</p>
@@ -61,6 +61,220 @@
             </div>
             <div class="ed-item">
                 <button>Read More</button>
+            </div>
+        </div>
+    </section>
+    <section class="ed-container">
+        <div class="ed-item ed-container">
+            <div class="ed-item">
+                <img class=icono src="assets/chef.svg" alt="">                
+            </div>
+            <div class="ed-item">
+                <p>Lorem</p>
+            </div>
+            <div class="ed-item">
+                <p>Lorem, ipsum.</p>
+            </div>
+        </div>
+        <div class="ed-item ed-container">
+            <div class="ed-container ed-item m-25">
+                <div class="ed-item">
+                    <img class=icono src="assets/chef.svg" alt="">
+                </div>
+                <div class="ed-item">
+                    <p>Lorem, ipsum.</p>
+                </div>
+                <div class="ed-item">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="ed-container ed-item m-25">
+                <div class="ed-item">
+                    <img class=icono src="assets/chef.svg" alt="">
+                </div>
+                <div class="ed-item">
+                    <p>Lorem, ipsum.</p>
+                </div>
+                <div class="ed-item">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="ed-container ed-item m-25">
+                <div class="ed-item">
+                    <img class=icono src="assets/chef.svg" alt="">
+                </div>
+                <div class="ed-item">
+                    <p>Lorem, ipsum.</p>
+                </div>
+                <div class="ed-item">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="ed-container ed-item m-25">
+                <div class="ed-item">
+                    <img class=icono src="assets/chef.svg" alt="">
+                </div>
+                <div class="ed-item">
+                    <p>Lorem, ipsum.</p>
+                </div>
+                <div class="ed-item">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit consectetur adipiscing elit.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="ed-container">
+        <div class="ed-container ed-item">
+            <div class="ed-item">
+                <img class=icono src="assets/chef.svg" alt="">
+            </div>
+            <div class="ed-item">
+                <p>Lorem, ipsum.</p>
+            </div>
+            <div class="ed-item">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit consectetur adipiscing elit.</p>
+            </div>
+        </div>
+        <div class="ed-container ed-item m-50">
+            <div class="ed-item m-35">
+                <img src="https://images.pexels.com/photos/428355/pexels-photo-428355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+            </div>
+            <div class="ed-container ed-item m-55">
+                <div class="ed-item">
+                    <p>Lorem, ipsum.</p>
+                </div>
+                <div class="ed-item">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ut aliquid ducimus ex, sapiente maiores voluptates cupiditate voluptati.</p>
+                </div>
+            </div>
+            <div class="ed-item m-5">
+                <p>$10</p>
+            </div>
+        </div>
+        <div class="ed-container ed-item m-50">
+            <div class="ed-item m-35">
+                <img src="https://images.pexels.com/photos/428355/pexels-photo-428355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+            </div>
+            <div class="ed-container ed-item m-55">
+                <div class="ed-item">
+                    <p>Lorem, ipsum.</p>
+                </div>
+                <div class="ed-item">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ut aliquid ducimus ex, sapiente maiores voluptates cupiditate voluptati.</p>
+                </div>
+            </div>
+            <div class="ed-item m-5">
+                <p>$10</p>
+            </div>
+        </div>
+        <div class="ed-container ed-item m-50">
+            <div class="ed-item m-35">
+                <img src="https://images.pexels.com/photos/428355/pexels-photo-428355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+            </div>
+            <div class="ed-container ed-item m-55">
+                <div class="ed-item">
+                    <p>Lorem, ipsum.</p>
+                </div>
+                <div class="ed-item">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ut aliquid ducimus ex, sapiente maiores voluptates cupiditate voluptati.</p>
+                </div>
+            </div>
+            <div class="ed-item m-5">
+                <p>$10</p>
+            </div>
+        </div>
+        <div class="ed-container ed-item m-50">
+            <div class="ed-item m-35">
+                <img src="https://images.pexels.com/photos/428355/pexels-photo-428355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+            </div>
+            <div class="ed-container ed-item m-55">
+                <div class="ed-item">
+                    <p>Lorem, ipsum.</p>
+                </div>
+                <div class="ed-item">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ut aliquid ducimus ex, sapiente maiores voluptates cupiditate voluptati.</p>
+                </div>
+            </div>
+            <div class="ed-item m-5">
+                <p>$10</p>
+            </div>
+        </div>
+        <div class="ed-container ed-item m-50">
+            <div class="ed-item m-35">
+                <img src="https://images.pexels.com/photos/428355/pexels-photo-428355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+            </div>
+            <div class="ed-container ed-item m-55">
+                <div class="ed-item">
+                    <p>Lorem, ipsum.</p>
+                </div>
+                <div class="ed-item">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ut aliquid ducimus ex, sapiente maiores voluptates cupiditate voluptati.</p>
+                </div>
+            </div>
+            <div class="ed-item m-5">
+                <p>$10</p>
+            </div>
+        </div>
+        <div class="ed-container ed-item m-50">
+            <div class="ed-item m-35">
+                <img src="https://images.pexels.com/photos/428355/pexels-photo-428355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+            </div>
+            <div class="ed-container ed-item m-55">
+                <div class="ed-item">
+                    <p>Lorem, ipsum.</p>
+                </div>
+                <div class="ed-item">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ut aliquid ducimus ex, sapiente maiores voluptates cupiditate voluptati.</p>
+                </div>
+            </div>
+            <div class="ed-item m-5">
+                <p>$10</p>
+            </div>
+        </div>
+    </section>
+    <section class="ed-container">
+        <div class="ed-item main-center">
+            <h2>Lorem, ipsum.</h2>
+        </div>
+        <div class="ed-item s-80 main-center">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis saepe ab deleniti libero vel atque sit, at architecto animi? In, consectetur vero atque eos aliquam architecto laudantium ea laborum corporis.</p>
+            <button>Book your table</button>
+        </div>
+    </section>
+    <section class="ed-container">
+        <div class="ed-item ed-container">
+            <div class="ed-item">
+                <img class=icono src="assets/chef.svg" alt="">                
+            </div>
+            <div class="ed-item">
+                <p>Lorem</p>
+            </div>
+            <div class="ed-item">
+                <p>Lorem, ipsum.</p>
+            </div>
+        </div>
+        <div class="ed-container ed-item m-30">
+            <div class="ed-item">
+                <p>Lorem, ipsum.</p>
+            </div>
+            <div class="ed-item">
+                <p>Lorem, ipsum.</p>
+            </div>
+        </div>
+        <div class="ed-container ed-item m-30">
+            <div class="ed-item">
+                <p>Lorem, ipsum.</p>
+            </div>
+            <div class="ed-item">
+                <p>Lorem, ipsum.</p>
+            </div>
+        </div>
+        <div class="ed-container ed-item m-30">
+            <div class="ed-item">
+                <p>Lorem, ipsum.</p>
+            </div>
+            <div class="ed-item">
+                <p>Lorem, ipsum.</p>
             </div>
         </div>
     </section>
